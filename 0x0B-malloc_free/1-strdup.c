f0b5a6b4f25c0d1049aed214f01af4d9321bce3e
treyvs <tj.jamestrevor@gmail.com>
2023-04-11 02:17:43 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -19,7 +19,7 @@ char *_strdup(char *str)
 	aaa = malloc(sizeof(char) * (i + 1));
 	if (aaa == NULL)
 		return (NULL);
-	for (r = 0; str[r]; r++
+	for (r = 0; str[r]; r++)
 		aaa[r] = str[r];
 	return (aaa);
 }
